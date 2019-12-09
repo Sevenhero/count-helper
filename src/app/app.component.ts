4466b8de3374299f2eb1e98bf2bd1c27f327a4fa
Sven Kernke <sven.kernke@yahoo.de>
2019-12-09 23:56:51 +0100

    added Httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from "@angular/core";
 import { Helper } from "./Helper";
 import { Month } from "./month";
+import { HttpClient } from '@angular/common/http';
+
+const baseUrl="http://localhost/indextest.php";
 
 @Component({
   selector: "app-root",
@@ -11,7 +14,12 @@ export class AppComponent {
   title = "count-helper";
   months: Month[] = [];
   editMode: boolean = false;
-  constructor() {
+  constructor(private http:HttpClient){
     this.months = new Helper().daysOfMonth();
+  }
+
+  async send(){
+    let data ={test:"mimi"}
+    await this.http.post(baseUrl,JSON.stringify(data)).toPromise();
   }
 }
